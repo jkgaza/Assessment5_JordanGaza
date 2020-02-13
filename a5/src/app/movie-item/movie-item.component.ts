2b3fc4be1c9698903fa457c75e4b099898983126
Jordan Gaza <Gaza>
2020-02-12 20:00:38 -0500

    Having troubles displaying, working on movie-list and movie-item

--- a5/src/app/movie-item/movie-item.component.ts
+++ a5/src/app/movie-item/movie-item.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IMovie } from '../imovie';
+import { MovieServiceService } from '../movie-service.service';
 
 @Component({
-  selector: 'app-movie-item',
+  selector: 'movie-item',
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.css']
 })
 export class MovieItemComponent implements OnInit {
+  @Input() movie : IMovie;
 
-  constructor() { }
+  constructor( public service : MovieServiceService) { }
 
   ngOnInit() {
   }
